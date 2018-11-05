7f265ff92d5afbed2d6bb784d12ec7365dd1bbe6
Shima <shima.jin@icloud.com>
2018-11-05 19:14:02 +0900

    Initial Commit

---
+++ PinCodeInputView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PinCodeInputView
+//
+//  Created by Jinsei Shima on 2018/11/05.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
