743e11ba8edf4d57d00c15a006e7649055297f3f
Shima <shima.jin@icloud.com>
2018-11-17 20:56:39 +0900

    Add carthage

---
+++ Carthage/Build/iOS/PinCodeInputView.framework/Headers/PinCodeInputView.h
@@ -0,0 +1,19 @@
+//
+//  PinCodeInputView.h
+//  PinCodeInputView
+//
+//  Created by Jinsei Shima on 2018/11/06.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PinCodeInputView.
+FOUNDATION_EXPORT double PinCodeInputViewVersionNumber;
+
+//! Project version string for PinCodeInputView.
+FOUNDATION_EXPORT const unsigned char PinCodeInputViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinCodeInputView/PublicHeader.h>
+
+
